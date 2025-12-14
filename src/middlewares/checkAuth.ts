3bc151fa834d241c59b0404e73407c5a48060e91
criperrr <gabriel.camaforte@unesp.br>
2025-12-14 20:34:04 -0300

    forget to add

---
+++ src/middlewares/checkAuth.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { AppError } from "../types/ApiError.js";
+import db, { queryOne, runSql } from "../utils/database.js";
+const secretKey: any = process.env.SECRETKEY;
+
+if (!secretKey) {
+    console.error("FATAL ERROR: SECRETKEY is not defined in .env");
+    process.exit(1);
+}
+
+export async function checkAuth(req: Request, res: Response, next: NextFunction) {
+    const jwtToken = req.headers.authorization!.split(" ")[1];
+    const masterToken = req.headers["x-master-token"] as string;
+
+    if (!jwtToken && !masterToken)
+        throw new AppError(
+            "No x-master-token OR JWTtoken provived",
+            401,
+            "AUTH_MISSING_CREDENTIALS"
+        );
+
+    if (jwtToken) {
+        jwt.verify(jwtToken, secretKey, function (err: Error | null) {
+            if (err) {
+                console.log(err);
+
+                if (err.name == "JsonWebTokenError") {
+                    throw new AppError("Invalid JWT token", 403, "AUTH_INVALID_TOKEN");
+                }
+            }
+
+            next();
+        });
+    }
+    if (masterToken) {
+        const user = await queryOne("SELECT 1 FROM Users WHERE masterToken = ?", [masterToken], db);
+        if (!user) throw new AppError("Invalid masterToken", 403, "AUTH_INVALID_TOKEN");
+        next();
+    }
+}
---
+++ src/models/users.ts
@@ -0,0 +1,10 @@
+export type Users = newUser & {
+    id_User?: number; // pra inserir no bd, que gera o id dps q eu enviar query
+    passwordHash: string;
+    masterToken: string;
+};
+export type newUser = {
+    name: string;
+    email: string;
+    password: string;
+};
